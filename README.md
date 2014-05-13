7a8099f559edd8b0067ce94f000a03b3433cd688
Sanel Deljkic <dsanel@gmail.com>
2014-05-13 14:44:22 +0200

    Folder and file documentation

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-This is testing
+ClineUiP 2
+===
+
+Folder and file structure:
+.
+├── app                          // Node.js server side code
+│   ├── controllers
+│   │   ├── index.js
+│   │   └── patients.js          // Actions for patient controller
+│   └── models
+│       ├── index.js
+│       └── patient.js           // MongoDB mapping file for "patient" collection
+├── app.json                     // Config
+├── index.js                     // Start index, db connection, routes...
+├── package.json                 // Config
+├── public                       // Frontend code (AngularJS, HTML, CSS)
+│   ├── css                      // Custom CSS lib files
+│   ├── index.html               // Home page, start page of our application
+│   ├── js
+│   │   ├── app.js
+│   │   ├── controllers
+│   │   │   ├── homeCtrl.js
+│   │   │   └── patientsCtrl.js  // Frontend code for 'patients' page
+│   │   ├── lib                  // Custom JS libs (jquery, angular, bootstrap...)
+│   │   └── services
+│   │       └── patients.js      // Frontend service to comunicate with server
+│   └── partials
+│       ├── header.html          // Navihation HTML
+│       ├── home.html            // Home page, 'Home Page...'
+│       └── patients.html        // HTML code for 'patient' page
+└── README.md
--- index.js
+++ index.js
@@ -15,62 +15,8 @@ for (var key in controllers) {
     app.use('/api/v1/' + key, controllers[key]().router);
 }
 
-app.get('/test-data', function(req, res){
-
-  var males = [
-    "Phillip Walker", "Adam Cox", "Jeremy Ross", "Jack Sanchez", "Martin Barnes",
-    "Carl Martinez", "Johnny Peterson", "Clarence Henderson", "Samuel Brown", "Frank Phillips"
-  ];
-
-  var females = [
-    "Ann Sanders", "Rachel Ramirez", "Nicole Scott", "Norma Walker", "Linda Ross",
-    "Andrea Bailey", "Martha Henderson", "Julia Washington", "Jessica Gonzalez", "Wanda Brooks"
-  ];
-
-  var idc = [
-    "H65.1", "K65.0", "A54.9", "A56.2", "B95.5", "B97.7", "D13.5", "D13.9", "E11.3", "E11.7",
-    "G11.1", "G11.8", "G40.3", "G40.9", "H30.2", "H31.4", "H34.2", "K55.1", "K56.1", "L20.8"
-  ];
-
-  Patient.find({}).remove(function () {
-    for (var i = 0; i < 10; i++){
-        var row = new Patient();
-
-        row.gender = Math.floor((Math.random() * 2) + 1) === 1 ? "male" : "female";
-        row.no = Math.floor((Math.random() * 8999) + 1000);
-        row.test1 = Math.random().toString(36).substring(7);
-        row.test2 = Math.random().toString(36).substring(7);
-        row.test3 = Math.random().toString(36).substring(7);
-        
-        if (row.gender === "male") {
-            var maleIndex = Math.floor(Math.random() * males.length);
-            row.name = males[maleIndex];
-            males.splice(maleIndex, 1);
-        } else {
-            var femaleIndex = Math.floor(Math.random() * females.length);
-            row.name = females[femaleIndex];
-            females.splice(femaleIndex, 1);
-        }
-
-        var count = Math.floor(Math.random() * 10) + 1;
-        for(var j = 0; j < count; j++) {
-            var detail = {
-                no: Math.floor((Math.random() * 899) + 100),
-                details: [ ]
-            };
-            var sCount = Math.floor(Math.random() * 8) + 4;
-            for(var s = 0; s < sCount; s++) {
-                detail.details.push(idc[Math.floor(Math.random() * idc.length)]);
-            }
-
-            row.details.push(detail);
-        }
-
-        row.save();
-    }
-});
-  res.send('ok');
-});
+// Temp code, create test data
+app.get("/test-data",function(a,b){var c=["Phillip Walker","Adam Cox","Jeremy Ross","Jack Sanchez","Martin Barnes","Carl Martinez","Johnny Peterson","Clarence Henderson","Samuel Brown","Frank Phillips"],d=["Ann Sanders","Rachel Ramirez","Nicole Scott","Norma Walker","Linda Ross","Andrea Bailey","Martha Henderson","Julia Washington","Jessica Gonzalez","Wanda Brooks"],e=["H65.1","K65.0","A54.9","A56.2","B95.5","B97.7","D13.5","D13.9","E11.3","E11.7","G11.1","G11.8","G40.3","G40.9","H30.2","H31.4","H34.2","K55.1","K56.1","L20.8"];Patient.find({}).remove(function(){for(var a=0;10>a;a++){var b=new Patient;if(b.gender=1===Math.floor(2*Math.random()+1)?"male":"female",b.no=Math.floor(8999*Math.random()+1e3),b.test1=Math.random().toString(36).substring(7),b.test2=Math.random().toString(36).substring(7),b.test3=Math.random().toString(36).substring(7),"male"===b.gender){var f=Math.floor(Math.random()*c.length);b.name=c[f],c.splice(f,1)}else{var g=Math.floor(Math.random()*d.length);b.name=d[g],d.splice(g,1)}for(var h=Math.floor(10*Math.random())+1,i=0;h>i;i++){for(var j={no:Math.floor(899*Math.random()+100),details:[]},k=Math.floor(8*Math.random())+4,l=0;k>l;l++)j.details.push(e[Math.floor(Math.random()*e.length)]);b.details.push(j)}b.save()}}),b.send("ok")});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
