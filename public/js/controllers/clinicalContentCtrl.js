1f61bd12c86da6fa2803935db51f8e3a14532852
Sanel Deljkic <sanel.deljkic@eneo.ba>
2014-10-07 15:16:52 +0200

    Work in progress

--- public/js/controllers/clinicalContentCtrl.js
+++ public/js/controllers/clinicalContentCtrl.js
@@ -4,36 +4,56 @@ angular.module('clinuip')
     .controller('ClinicalContentCtrl', function UsersCtrl($scope, $location, $routeParams) {
 
         $scope.contents = [
-            { tags: 'jedan, dva, tri', content: 'prvi red\ndrugired' },
-            { tags: 'jedan, dva, tri, dsfsdfsd, sdfsdf, sdfsdf', content: 'prvi red\ndrugired' },
-            { tags: 'jedan, dva, tri', content: 'prvi red\ndrugired\ndrugired\ndrugired' }
+            { id: 1, tags: ['jedan', 'dva', 'tri'], content: 'prvi red\ndrugired' },
+            { id: 2, tags: ['aaaa', 'bbbb', 'ccc'], content: 'prvi red\ndrugired' },
+            { id: 3, tags: ['jedan', 'dva', 'tri'], content: 'prvi red\ndrugired\ndrugired\ndrugired' }
         ];
 
-        $scope.addContent = function () {
-            var htmlForm = $(".form-add-content").clone().show();
+        $scope.current = {};
 
-            showModal(htmlForm, "Add medical content details", function () {
+        $scope.saveContent = function (content) {
+            content.tags = _.map($("#tags").tokenfield('getTokens'), function(item) { return item.value; });
 
-            });
+            if (content.id) {
+                var index = _.findIndex($scope.contents, { id : content.id });
+                if (index !== -1) {
+                    $scope.contents[index] = content;
+                }
+            } else {
+                content.id = _.random(10, 99);
+                $scope.contents.push( angular.copy(content));
+            }
+            $('#myModal').modal('hide');
         };
 
-        // Private util method
-        function showModal(html, title, callback) {
-            bootbox.dialog({
-                message: html,
-                title: title,
-                buttons: {
-                    success: {
-                        label: "Cancel",
-                        className: "btn btn-default"
-                    },
-                    main: {
-                        label: "Save",
-                        className: "btn-primary",
-                        callback: callback
-                    }
+        $scope.editContent = function (content) {
+            $scope.current = angular.copy(content);
+            $("#tags").val($scope.current.tags.join(', ')).tokenfield({
+                autocomplete: {
+                    delay: 100
                 }
             });
-        }
+            $('#myModal').modal();
+        };
+
+        $scope.deleteContent = function (content) {
+            if (content.id) {
+                var index = _.findIndex($scope.contents, { id : content.id });
+                if (index !== -1) {
+                    $scope.contents.splice(index, 1);
+                }
+            }
+        };
+
+        $scope.addContent = function () {
+            $scope.current = {};
+            $("#tags").val('').tokenfield({
+                autocomplete: {
+                    delay: 100
+                }
+            });
+            $('#myModal').modal();
+        };
+
 
     });
